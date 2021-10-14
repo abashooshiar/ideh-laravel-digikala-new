97b0b3258d7c226a5aefaee47ad5b613ebe1264f
abashooshiar <hooshiar.abas@gmail.com>
2021-10-14 07:00:38 +0200

    Category dars 4_1

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
     protected $table = 'category';
     protected $fillable = ['name', 'ename', 'parent_id', 'url', 'search_url', 'img', 'notShow'];
+
+    public static function get_parent()
+    {
+        $array = [0 => 'Hauptkategorie'];
+        $list = self::with('getChild.getChild')->where('parent_id', 0)->get();
+        foreach ($list as $key => $value) {
+            $array[$value->id] = $value->name;
+            foreach ($value->getChild as $key2 => $value2) {
+                $array[$value2->id] = ' - ' . $value2->name;
+                foreach ($value2->getChild as $key3 => $value3) {
+                    $array[$value3->id] = ' - - ' . $value3->name;
+                }
+            }
+        }
+        return $array;
+    }
+
+    public function getChild()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+    public function getParent()
+    {
+        return $this->hasOne(Category::class, 'id', 'parent_id')->withTrashed()->withDefault(['name' => '-']);
+    }
 }
+
